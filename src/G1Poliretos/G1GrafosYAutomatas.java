c65b69053c440aae753e528cf8d22ac375ca71bf
Anthony Alangasi <anthony.alan810@gmail.com>
2024-11-04 17:48:35 -0500

    Correcciones

--- src/G1Poliretos/G1GrafosYAutomatas.java
+++ src/G1Poliretos/G1GrafosYAutomatas.java
@@ -16,9 +16,9 @@ public class G1GrafosYAutomatas {
 
     final String alfabeto2 = "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z-1-2-3-4-5-6-7-8-9-_-$-;"; 
     final int var_valida = 100,
-          matriz_transicion2[][] = {// a       b        c      d        e       f         g         h    i       j        k        l     m       n        o     p      q     r      s        t        u      v      w         x    y       z       1        2       3       4         5     6       7        8       9       _      $         ;
-                                     {1      ,1       ,1      ,1      ,1       ,1        ,1         ,1   ,1      ,1      ,1       ,1    ,1      ,1        ,1      ,1   ,1    ,1      ,1       ,1    ,1     ,1       ,1       ,1   ,1      ,1      ,err    ,err  ,err  ,err  ,err  ,err  ,err  ,err  ,err  ,err  ,1       ,err       }  //q0
-                                    ,{1      ,1       ,1      ,1      ,1       ,1        ,1         ,1   ,1      ,1      ,1       ,1    ,1      ,1        ,1      ,1   ,1    ,1      ,1       ,1    ,1     ,1       ,1       ,1   ,1      ,1      ,1       ,1     ,1     ,1     ,1     ,1     ,1    ,1      ,1      ,1      ,1      ,var_valida }//q1
+          matriz_transicion2[][] = {// a       b       c       d       e        f         g         h        i         j         k        l       m       n       o        p        q       r        s        t       u      v       w        x       y       z       1          2        3         4         5         6         7        8       9         _        $         ;
+                                     {1      ,1       ,1      ,1      ,1       ,1        ,1        ,1        ,1        ,1       ,1       ,1      ,1      ,1       ,1      ,1       ,1      ,1       ,1       ,1      ,1      ,1      ,1      ,1      ,1      ,1      ,err      ,err      ,err      ,err      ,err      ,err      ,err     ,err     ,err     ,1       ,1      ,err        }  //q0
+                                    ,{1      ,1       ,1      ,1      ,1       ,1        ,1        ,1        ,1        ,1       ,1       ,1      ,1      ,1       ,1      ,1       ,1      ,1       ,1       ,1      ,1      ,1      ,1      ,1      ,1      ,1      ,1        ,1        ,1        ,1        ,1        ,1        ,1       ,1       ,1       ,1       ,1      ,var_valida }  //q1
                                 };
 
     @SuppressWarnings("resource")
